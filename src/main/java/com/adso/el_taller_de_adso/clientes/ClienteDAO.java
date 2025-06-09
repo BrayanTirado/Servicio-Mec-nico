811028d74cce7163fb8befc607c91daf780ad276
axchisan <leftyrancuentabot@gmail.com>
2025-06-08 21:38:40 -0500

    agregue la funcion registrar clientes al DAO, porque parece que antes no se cargo

--- src/main/java/com/adso/el_taller_de_adso/clientes/ClienteDAO.java
+++ src/main/java/com/adso/el_taller_de_adso/clientes/ClienteDAO.java
@@ -4,10 +4,31 @@
  */
 package com.adso.el_taller_de_adso.clientes;
 
+import com.adso.el_taller_de_adso.ConexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author axchisan
  */
 public class ClienteDAO {
-    
+    public void registrarcliente(Cliente cliente){
+        String sql = "INSERT INTO clientes (nombre, documento, tlefono, correo, rol) VALUES (?, ?, ?, ?, ?)";
+        try(Connection conn = ConexionBD.conectar();
+           PreparedStatement stmt = conn.prepareStatement(sql)){
+           stmt.setString(1, cliente.getNombre());
+           stmt.setString(2, cliente.getDocumento());
+           stmt.setString(3, cliente.getTelefono());
+           stmt.setString(4, cliente.getCorreo());
+           stmt.setInt(5, cliente.getRol());
+           stmt.executeUpdate();
+           JOptionPane.showMessageDialog(null, "Usuario registrado con exito");
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al registrar cliente: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            
+        }
+    }
 }
